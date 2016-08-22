db1aac4eac8b83470165de48a89d10334c81755d
Vinícius dos Santos Oliveira <vinipsmaker@maidsafe.net>
2016-08-22 15:56:41 +0000

    Change callback to be less error-prone
    Properly wait for the callback to be called.

--- js/index.js
+++ js/index.js
@@ -6,14 +6,26 @@ var lib = ffi.Library('./libffi_async_demo', {
   'run_delayed': [ 'void', [ 'pointer' ] ],
 });
 
-var callback = ffi.Callback('void', [], function() {
+var Callback = function(retType, argTypes, abi, func) {
+    if (typeof abi === 'function') {
+        func = abi
+        abi = void(0)
+    }
+
+    var timeout = setInterval(function() {}, 500);
+
+    var wrapper = function(res) {
+        clearInterval(timeout);
+        func(res);
+    };
+
+    return ffi.Callback(retType, argTypes, abi, wrapper);
+}
+
+var callback = Callback('void', [], function() {
   console.log("js callback started");
   lib.print_thread_id()
   console.log("js callback finished");
 })
 
 lib.run_delayed(callback)
-
-setTimeout(function() {
-  console.log("js main finished")
-}, 2000)
