41c8aebff9650d9bbe11e00aafe0bd03cf11d7bd
Vinícius dos Santos Oliveira <vinipsmaker@maidsafe.net>
2017-01-17 17:11:03 +0000

    More

--- js/index.js
+++ js/index.js
@@ -1,9 +1,55 @@
+"use strict";
 var ffi = require('ffi'),
-    ref = require('ref')
+    ref = require('ref');
+var StructType = require('ref-struct');
+var Enum = require('enum');
+
+const size_t = ref.types.size_t;
+const int32 = ref.types.int32;
+const Int = ref.types.int;
+const int64 = ref.types.int64;
+const u8 = ref.types.uint8;
+const u64 = ref.types.uint64;
+const Void = ref.types.void;
+
+const VoidPointer = ref.refType(Void);
+const PointerToVoidPointer = ref.refType(ref.refType(Void));
+const u8Pointer = ref.refType(u8);
+const u64Pointer = ref.refType(u64);
+const PointerToU8Pointer = ref.refType(u8Pointer);
+
+const FileMetadata = new StructType({
+  name: u8Pointer,
+  name_len: size_t,
+  name_cap: size_t,
+  user_metadata: u8Pointer,
+  user_metadata_len: size_t,
+  user_metadata_cap: size_t,
+  size: u64,
+  creation_time_sec: int64,
+  creation_time_nsec: int64,
+  modification_time_sec: int64,
+  modification_time_nsec: int64
+});
+
+const FileDetails = new StructType({
+  content: u8Pointer,
+  content_len: size_t,
+  content_cap: size_t,
+  metadata: ref.refType(FileMetadata)
+});
+
+const FileDetailsHandle = ref.refType(FileDetails);
+const PointerToFileDetailsPointer = ref.refType(FileDetailsHandle);
 
 var lib = ffi.Library('./libffi_async_demo', {
   'print_thread_id': [ 'void', [] ],
   'run_delayed': [ 'void', [ 'pointer' ] ],
+  'get_shit_done': ['void', [PointerToFileDetailsPointer]],
+  'get_shit_done2': ['void', [PointerToVoidPointer]],
+  'get_shit_done3': ['void', [PointerToVoidPointer]],
+  'get_shit_done4': ['void', [ref.refType(Int)]],
+  'drop_vector': ['void', [u8Pointer, 'size_t', 'size_t']]
 });
 
 var Callback = function(retType, argTypes, abi, func) {
@@ -22,10 +68,26 @@ var Callback = function(retType, argTypes, abi, func) {
     return ffi.Callback(retType, argTypes, abi, wrapper);
 }
 
-var callback = Callback('void', [], function() {
+/*var callback = Callback('void', [], function() {
   console.log("js callback started");
   lib.print_thread_id()
   console.log("js callback finished");
 })
+lib.run_delayed(callback)*/
 
-lib.run_delayed(callback)
+/*const fileDetailsPointer = ref.alloc(FileDetailsHandle, ref.NULL);
+console.log(fileDetailsPointer);
+lib.get_shit_done(fileDetailsPointer);
+console.log(fileDetailsPointer.deref().address().toString(16));
+let f2 = new FileDetails(fileDetailsPointer.deref());
+console.log(f2.content_len);*/
+
+/*const handle = ref.alloc(VoidPointer, ref.NULL);
+lib.get_shit_done2(handle);
+lib.get_shit_done3(handle.deref());*/
+
+const Ffi_FilterType = new Enum({'BlackList': 0, 'WhiteList': 1});
+
+let filterType = ref.alloc(Int);
+lib.get_shit_done4(filterType);
+console.log(Ffi_FilterType.get(filterType.deref()).key);
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,82 @@ extern crate thread_id;
 use std::thread;
 use std::time::Duration;
 
+#[derive(Debug)]
+#[repr(C)]
+pub struct FileMetadata {
+    pub name: *mut u8,
+    pub name_len: usize,
+    pub name_cap: usize,
+    pub user_metadata: *mut u8,
+    pub user_metadata_len: usize,
+    pub user_metadata_cap: usize,
+    pub size: u64,
+    pub creation_time_sec: i64,
+    pub creation_time_nsec: i64,
+    pub modification_time_sec: i64,
+    pub modification_time_nsec: i64,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct FileDetails {
+    /// Content of the file, base64 encoded string.
+    pub content: *mut u8,
+    /// Size of `content`
+    pub content_len: usize,
+    /// Capacity of `content`. Only used by the allocator's `dealloc` algorithm.
+    pub content_cap: usize,
+    /// Metadata of the file.
+    pub metadata: *mut FileMetadata,
+}
+
 #[no_mangle]
 pub extern fn print_thread_id() {
     println!("current thread id: {}", thread_id::get())
+}
+
+#[no_mangle]
+pub unsafe fn get_shit_done(o_data: *mut *mut FileDetails) {
+    println!("Rust: {:p}", *o_data as *const _);
+    let response = FileDetails {
+        content: 0 as *mut u8,
+        content_len: 123654,
+        content_cap: 741852,
+        metadata: 0 as *mut _,
+    };
+    *o_data = Box::into_raw(Box::new(response));
+    println!("Rust: {:p}", o_data as *const _);
+    println!("Rust: {:p}", *o_data as *const _);
+}
+
+#[no_mangle]
+pub unsafe fn get_shit_done2(o_data: *mut *mut i32) {
+    *o_data = Box::into_raw(Box::new(42));
+}
+
+#[no_mangle]
+pub unsafe fn get_shit_done3(handle: *mut i32) {
+    println!("{}", *handle);
+}
+
+#[repr(C)]
+#[derive(Debug, PartialEq)]
+pub enum FilterType {
+    /// BlackList
+    BlackList,
+    /// WhiteList
+    WhiteList,
+}
+
+#[no_mangle]
+pub unsafe fn get_shit_done4(o_data: *mut FilterType) {
+    *o_data = FilterType::BlackList;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn drop_vector(ptr: *mut u8, size: usize,
+                                     capacity: usize) {
+    let _ = Vec::from_raw_parts(ptr, size, capacity);
 }
 
 #[no_mangle]
